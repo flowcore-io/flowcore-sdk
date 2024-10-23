5f8a6bc0db7a64d9cea0a0064a61b2c73e7530f8
René Bischoff <rene.bischoff@gmail.com>
2024-10-23 22:35:48 +0200

    stuff

--- src/utils/parse-response.ts
+++ src/utils/parse-response.ts
@@ -2,7 +2,7 @@ import { Value } from "@sinclair/typebox/value"
 import type { Static, TSchema } from "@sinclair/typebox"
 
 export const parseResponse = <T extends TSchema>(schema: T, response: unknown): Static<T> => {
-  console.log("response", response)
+  console.log("response", response, schema)
   if (!Value.Check(schema, response)) {
     console.log("invalid response")
     const errors = Value.Errors(schema, response)
