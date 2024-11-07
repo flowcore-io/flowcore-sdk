c3ac222613ca82c6e8ba364ca6ffc67b356b6a3f
René Bischoff <rene.bischoff@gmail.com>
2024-11-07 12:57:47 +0100

    fix: expose command class + add v2 events commands

---
+++ src/commands/v2/events/events.fetch-time-buckets-by-name.ts
@@ -0,0 +1,78 @@
+import { Type } from "@sinclair/typebox"
+import { Command } from "../../../common/command.ts"
+import { parseResponseHelper } from "../../../utils/parse-response-helper.ts"
+
+/**
+ * The input for the events fetch indexes command
+ */
+export interface V2EventsFetchTimeBucketsByNameInput {
+  /** the tenant name */
+  tenant: string
+  /** the data core id */
+  dataCoreId: string
+  /** the flow type name */
+  flowType: string
+  /** the event type name */
+  eventType: string
+  /** the paging cursor */
+  cursor?: number
+  /** the page size (default is 10.000) */
+  pageSize?: number
+  /** start from this time bucket */
+  fromTimeBucket?: string
+  /** end at this time bucket */
+  toTimeBucket?: string
+}
+
+/**
+ * The output for the events fetch indexes command
+ */
+export interface V2EventsFetchTimeBucketsByNameOutput {
+  /** the time buckets */
+  timeBuckets: string[]
+  /** the next page cursor */
+  nextCursor?: string
+}
+
+const responseSchema = Type.Object({
+  timeBuckets: Type.Array(Type.String()),
+  nextCursor: Type.Optional(Type.String()),
+})
+
+/**
+ * Fetch time buckets for an event type
+ */
+export class V2EventsFetchTimeBucketsByNameCommand
+  extends Command<V2EventsFetchTimeBucketsByNameInput, V2EventsFetchTimeBucketsByNameOutput> {
+  /**
+   * Get the base url for the request
+   */
+  protected override getBaseUrl(): string {
+    return "https://event-source.api.flowcore.io"
+  }
+  /**
+   * Get the path for the request
+   */
+  protected override getPath(): string {
+    return `/api/v1/time-buckets/byName/${this.input.tenant}/${this.input.dataCoreId}/${this.input.flowType}/${this.input.eventType}`
+  }
+  /**
+   * Parse the response
+   */
+  protected override parseResponse(rawResponse: unknown): V2EventsFetchTimeBucketsByNameOutput {
+    const response = parseResponseHelper(responseSchema, rawResponse)
+    return response
+  }
+
+  /**
+   * Get the body for the request
+   */
+  protected override getBody(): string {
+    return JSON.stringify({
+      cursor: this.input.cursor,
+      pageSize: this.input.pageSize,
+      fromTimeBucket: this.input.fromTimeBucket,
+      toTimeBucket: this.input.toTimeBucket,
+    })
+  }
+}
