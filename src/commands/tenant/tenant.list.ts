0cbf9f5c8d526c1d1ebe2d22624f02c603115130
flowcore-platform <gitops@flowcore.io>
2025-02-14 11:04:33 +0000

    fix: handle error response in tenant list

--- src/commands/tenant/tenant.list.ts
+++ src/commands/tenant/tenant.list.ts
@@ -2,6 +2,7 @@ import { Type } from "@sinclair/typebox"
 import { GraphQlCommand } from "../../common/command.ts"
 import { TenantV0Schema, tenantV0ToTenant, type TenantWithLinkType } from "../../contracts/tenant.ts"
 import { parseResponseHelper } from "../../utils/parse-response-helper.ts"
+import { CommandError } from "../../exceptions/command-error.ts"
 
 const graphQlQueryById = `
   query FLOWCORE_SDK_TENANT_LIST {
@@ -21,20 +22,29 @@ const graphQlQueryById = `
 `
 
 const responseSchema = Type.Object({
-  data: Type.Object({
-    me: Type.Object({
-      organizations: Type.Array(Type.Object({
-        linkType: Type.Union([Type.Literal("OWNER"), Type.Literal("COLLABORATOR")]),
-        organization: TenantV0Schema,
-      })),
-    }),
-  }),
+  data: Type.Union(
+    [
+      Type.Object({
+        me: Type.Object({
+          organizations: Type.Array(Type.Object({
+            linkType: Type.Union([Type.Literal("OWNER"), Type.Literal("COLLABORATOR")]),
+            organization: TenantV0Schema,
+          })),
+        }),
+      }),
+      Type.Null(),
+    ],
+  ),
+  errors: Type.Optional(Type.Array(Type.Object({
+    message: Type.String(),
+    path: Type.Optional(Type.Array(Type.String())),
+  }))),
 })
 
 /**
  * List tenants
  */
-export class TenantListCommand extends GraphQlCommand<Record<string, never>, TenantWithLinkType[]> {
+export class TenantListCommand extends GraphQlCommand<void, TenantWithLinkType[]> {
   /**
    * The allowed modes for the command
    */
@@ -45,6 +55,11 @@ export class TenantListCommand extends GraphQlCommand<Record<string, never>, Ten
    */
   protected override parseResponse(rawResponse: unknown): TenantWithLinkType[] {
     const response = parseResponseHelper(responseSchema, rawResponse)
+    if (response.errors?.length) {
+      throw new CommandError(this.constructor.name, response.errors.map((error) => error.message).join("; "))
+    } else if (!response.data) {
+      throw new CommandError(this.constructor.name, "No data returned from the command")
+    }
     return response.data.me.organizations.flatMap((organization) => ({
       ...tenantV0ToTenant(organization.organization),
       linkType: organization.linkType,
