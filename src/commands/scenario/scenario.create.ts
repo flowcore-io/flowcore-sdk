3b6433ebb7b85caffc8eae8d99d60a4adc1b96a1
flowcore-platform <gitops@flowcore.io>
2025-05-13 18:23:26 +0100

    feat(scenario): :tada: add create and list scenario commands with schema

---
+++ src/commands/scenario/scenario.create.ts
@@ -0,0 +1,55 @@
+import { Command } from "../../common/command.ts"
+import { type Scenario, ScenarioSchema } from "../../contracts/scenario.ts"
+import { parseResponseHelper } from "../../utils/parse-response-helper.ts"
+
+
+/**
+ * The input for the scenario create command
+ */
+export interface ScenarioCreateInput {
+  /** The tenant id */
+  tenantId: string
+  /** The name of the scenario */
+  name: string
+  /** The description of the scenario */
+  description?: string
+  /** The display name of the scenario */
+  displayName?: string
+}
+
+/**
+ * Create a scenario
+ */
+export class ScenarioCreateCommand extends Command<ScenarioCreateInput, Scenario> {
+  /**
+   * Whether the command should retry on failure
+   */
+  protected override retryOnFailure: boolean = false
+
+  /**
+   * Get the method
+   */
+  protected override getMethod(): string {
+    return "POST"
+  }
+  /**
+   * Get the base url
+   */
+  protected override getBaseUrl(): string {
+    return "https://scenario-2.api.flowcore.io"
+  }
+
+  /**
+   * Get the path
+   */
+  protected override getPath(): string {
+    return `/api/v1/scenarios`
+  }
+
+  /**
+   * Parse the response
+   */
+  protected override parseResponse(rawResponse: unknown): Scenario {
+    return parseResponseHelper(ScenarioSchema, rawResponse)
+  }
+}
---
+++ src/contracts/scenario.ts
@@ -0,0 +1,40 @@
+import {
+  type Static,
+  type TObject,
+  type TOptional,
+  type TString,
+  Type
+} from "@sinclair/typebox"
+
+/**
+ * The schema for an event type
+ */
+export const ScenarioSchema: TObject<{
+  id: TString
+  tenantId: TString
+  name: TString
+  displayName: TOptional<TString>
+  description: TOptional<TString>
+  createdAt: TString
+  updatedAt: TString
+}> = Type.Object({
+  /** Unique identifier for the event type */
+  id: Type.String(),
+  /** ID of the tenant that owns this event type */
+  tenantId: Type.String(),
+  /** Name of the event type */
+  name: Type.String(),
+  /** Display name of the event type */
+  displayName: Type.Optional(Type.String()),
+  /** Description of the event type */
+  description: Type.Optional(Type.String()),
+  /** Creation timestamp */
+  createdAt: Type.String(),
+  /** Last update timestamp */
+  updatedAt: Type.String(),
+})
+
+/**
+ * The type for an event type
+ */
+export type Scenario = Static<typeof ScenarioSchema>
