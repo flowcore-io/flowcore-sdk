512009bcb4efb94d5ee7a3ca95465d0ab43fbeaa
odinellefsen <odin@ellefsen.fo>
2025-06-23 09:38:31 +0100

    refactor(iam): enhance PolicySchema and PolicyStatementSchema with TypeBox static types for improved type safety

--- src/commands/iam/policies/create-policy.ts
+++ src/commands/iam/policies/create-policy.ts
@@ -1,11 +1,15 @@
 // src/contracts/policy.ts
 import { Command, parseResponseHelper } from "@flowcore/sdk"
-import { type Static, Type } from "@sinclair/typebox"
+import { type Static, Type, type TObject, type TString, type TArray, type TUnion, type TOptional, type TBoolean } from "@sinclair/typebox"
 
 /**
  * The schema for a policy statement document
  */
-export const PolicyStatementSchema = Type.Object({
+export const PolicyStatementSchema: TObject<{
+  statementId: TOptional<TString>
+  resource: TString
+  action: TUnion<[TString, TArray<TString>]>
+}> = Type.Object({
   statementId: Type.Optional(Type.String()),
   resource: Type.String(),
   action: Type.Union([Type.String(), Type.Array(Type.String())]),
@@ -14,7 +18,18 @@ export const PolicyStatementSchema = Type.Object({
 /**
  * The schema for a policy
  */
-export const PolicySchema = Type.Object({
+export const PolicySchema: TObject<{
+  id: TString
+  organizationId: TString
+  name: TString
+  version: TString
+  policyDocuments: TArray<typeof PolicyStatementSchema>
+  description: TOptional<TString>
+  principal: TOptional<TString>
+  flowcoreManaged: TBoolean
+  archived: TOptional<TBoolean>
+  frn: TString
+}> = Type.Object({
   id: Type.String(),
   organizationId: Type.String(),
   name: Type.String(),
