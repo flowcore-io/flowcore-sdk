12f14f2d486ce23dd69284a000fbfd06e615ce15
odinellefsen <odin@ellefsen.fo>
2026-01-19 10:33:52 +0000

    feat: add UserDeleteCommand to handle deletion of the authenticated user

---
+++ src/commands/user/user.delete.ts
@@ -0,0 +1,64 @@
+import { Type } from "@sinclair/typebox"
+import { Command } from "../../common/command.ts"
+import { parseResponseHelper } from "../../utils/parse-response-helper.ts"
+import type { Static, TObject, TString } from "@sinclair/typebox"
+
+/**
+ * The input for initializing user in Keycloak
+ * No input parameters needed - checks current authenticated user
+ */
+export type UserDeleteInput = Record<PropertyKey, never>
+
+/**
+ * The output for initializing user in Keycloak
+ * (matches the REST API response)
+ */
+export type UserDeleteOutput = Static<typeof responseSchema>
+
+const responseSchema: TObject<{
+  id: TString
+}> = Type.Object({
+  id: Type.String(),
+})
+
+/**
+ * Delete current authenticated user
+ */
+export class UserDeleteCommand extends Command<
+  UserDeleteInput,
+  UserDeleteOutput
+> {
+  /**
+   * The allowed modes for the command
+   */
+  protected override allowedModes: ("apiKey" | "bearer")[] = ["bearer"]
+
+  /**
+   * Get the base URL for the request
+   */
+  protected override getBaseUrl(): string {
+    return "https://user-2.api.flowcore.io"
+  }
+
+  /**
+   * Get the method
+   */
+  protected override getMethod(): string {
+    return "DELETE"
+  }
+
+  /**
+   * Get the path for the request
+   */
+  protected override getPath(): string {
+    return `/api/users/`
+  }
+
+  /**
+   * Parse the response
+   */
+  protected override parseResponse(rawResponse: unknown): UserDeleteOutput {
+    const response = parseResponseHelper(responseSchema, rawResponse)
+    return response
+  }
+}
