b721c28e61f49bc951f104d4ce87232b947ad99c
odinellefsen <odin@ellefsen.fo>
2026-01-19 10:42:05 +0000

    feat: add UserDeleteCommand and corresponding tests for user deletion functionality

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -87,6 +87,7 @@ export * from "./security/permissions.list.ts"
 
 // User
 export * from "./user/user.initialize-in-keycloak.ts"
+export * from "./user/user.delete.ts"
 export * from "./user/user.invite-to-tenant.ts"
 
 // Scenario
